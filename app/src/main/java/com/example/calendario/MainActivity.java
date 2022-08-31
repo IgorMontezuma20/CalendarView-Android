9b9b7e25acb2d7ff48376ea54dff0ecb5eb9a38a
imiranda <igor.miranda@foursys.com.br>
2022-08-31 14:26:25 -0300

    utilizando calendarView

--- app/src/main/java/com/example/calendario/MainActivity.java
+++ app/src/main/java/com/example/calendario/MainActivity.java
@@ -8,6 +8,10 @@ import android.util.Log;
 import android.widget.CalendarView;
 
 import com.example.calendario.databinding.ActivityMainBinding;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,33 @@ public class MainActivity extends AppCompatActivity {
             }
         }); */
 
+//        binding.calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+//            @Override
+//            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+//                Log.i("data: ","valor: " + date);
+//            }
+//        });
+
+//        binding.calendarView.state().edit()
+//                        .setMinimumDate(CalendarDay.from(2015,1, 1))
+//                        .setMaximumDate(CalendarDay.from(2022, 12, 31))
+//                        .commit();
+
+        CharSequence meses[] = {"Janeiro", "Fevreiro", "Março", "Abril", "Maio", "Junho", "Julho",
+        "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+        CharSequence semanas[] = {"Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"};
+
+
+        binding.calendarView.setTitleMonths(meses);
+        binding.calendarView.setWeekDayLabels(semanas);
+
+        binding.calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Log.i("data: ","valor: " + (date.getMonth()+1) +"/" + date.getYear());
+            }
+        });
 
     }
 }
